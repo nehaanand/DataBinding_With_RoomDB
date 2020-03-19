4ce816897d6917f9568354db1029b9737270d544
Neha Anand <nehaanand0801@gmail.com>
2020-03-19 17:25:52 +0530

    Room Database Setup and Record Insertion

--- app/src/main/java/com/neha/butterknife/Click_Events.java
+++ app/src/main/java/com/neha/butterknife/Click_Events.java
@@ -6,6 +6,5 @@ import android.widget.Toast;
 
 public interface Click_Events {
 
-
-    public void on_Register_Button_Click(View view);
+    void on_Register_Button_Click();
 }
--- app/src/main/java/com/neha/butterknife/Register_Screen.java
+++ app/src/main/java/com/neha/butterknife/Register_Screen.java
@@ -2,8 +2,10 @@ package com.neha.butterknife;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.room.Database;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,37 +13,86 @@ import android.widget.Toast;
 
 
 import com.neha.butterknife.databinding.ActivityRegisterScreenBinding;
+import com.neha.butterknife.entity.Login_Register_Entity;
 
 import org.json.JSONObject;
 
 public class Register_Screen extends AppCompatActivity implements Click_Events {
 
-    ActivityRegisterScreenBinding register_binding;
+    ActivityRegisterScreenBinding registerScreenBinding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        register_binding = DataBindingUtil.setContentView(Register_Screen.this, R.layout.activity_register__screen);
+        registerScreenBinding = DataBindingUtil.setContentView(this, R.layout.activity_register__screen);
+        registerScreenBinding.setClickEvents(Register_Screen.this);
+
     }
 
     @Override
-    public void on_Register_Button_Click(View view) {
-        Toast.makeText(Register_Screen.this, "Register clicked!" + register_binding.etEmail.getText().toString(), Toast.LENGTH_LONG).show();
-
+    public void on_Register_Button_Click() {
+        Toast.makeText(Register_Screen.this, "Register clicked!  " + registerScreenBinding.etEmail.getText().toString(), Toast.LENGTH_LONG).show();
+        save_user();
 
     }
 
-//    public class Items_Click_listener {
-//        Context context;
-//
-//        public Items_Click_listener(Context context) {
-//            this.context = context;
-//        }
-//        public void on_Register_Button_Click(View view) {
-//            Toast.makeText(context, "Register clicked!"+register_binding.etEmail.getText().toString(), Toast.LENGTH_LONG).show();
-//        }
-//    }
+
+    public void save_user(){
+        if (registerScreenBinding.etFirstName.getText().toString().isEmpty()) {
+            registerScreenBinding.etFirstName.setError("Field Required");
+            registerScreenBinding.etFirstName.requestFocus();
+            return;
+        }
+        if (registerScreenBinding.etLastName.getText().toString().isEmpty()) {
+            registerScreenBinding.etLastName.setError("Field Required");
+            registerScreenBinding.etLastName.requestFocus();
+            return;
+        }
+
+        if (registerScreenBinding.etEmail.getText().toString().isEmpty()) {
+            registerScreenBinding.etEmail.setError("Field Required");
+            registerScreenBinding.etEmail.requestFocus();
+            return;
+        }
+
+        if (registerScreenBinding.etPassword.getText().toString().isEmpty()) {
+            registerScreenBinding.etPassword.setError("Field Required");
+            registerScreenBinding.etPassword.requestFocus();
+            return;
+        }
+
+
+        class Save_User_To_Room extends AsyncTask<Void,Void,Void>{
+            @Override
+            protected Void doInBackground(Void... voids) {
+                Login_Register_Entity entity = new Login_Register_Entity();
+                entity.setEmail(registerScreenBinding.etEmail.getText().toString());
+                entity.setFirstName(registerScreenBinding.etFirstName.getText().toString());
+                entity.setLastName(registerScreenBinding.etLastName.getText().toString());
+                entity.setPassword(registerScreenBinding.etPassword.getText().toString());
+
+                //adding to database
+                Database_Client.getInstance(getApplicationContext()).getAppDatabase()
+                        .dao_obj()
+                        .insert(entity);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                finish();
+                Toast.makeText(getApplicationContext(), "Record Inserted", Toast.LENGTH_LONG).show();
+            }
+        }
+
+
+        Save_User_To_Room st = new Save_User_To_Room();
+        st.execute();
+    }
+
 }
 
 
